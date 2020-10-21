c00ca927dfb68b0e39acc0111dae352e90a7a4af
joseluisgs <joseluisgonsan@gmail.com>
2020-10-21 19:04:16 +0200

    Liberías Gradle

--- app/src/main/java/com/joseluisgs/todocamara2020/MainActivity.kt
+++ app/src/main/java/com/joseluisgs/todocamara2020/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.joseluisgs.todocamara2020
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        iniciarUI()
     }
+
+    /**
+     * Inicia la interfaz y los eventos de la apliación
+     */
+    private fun iniciarUI() {
+        // Iniciamos los permisos
+        iniciarPermisos()
+    }
+
+    /**
+     * Comprobamos los permisos de la aplicación
+     */
+    private fun iniciarPermisos() {
+        TODO("Not yet implemented")
+    }
+
+
 }
