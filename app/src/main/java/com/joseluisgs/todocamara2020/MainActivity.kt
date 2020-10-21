71f76b6260d90f4c37b0c799c1f2baf8ca75d29a
joseluisgs <joseluisgonsan@gmail.com>
2020-10-21 19:11:30 +0200

    init Permisos

--- app/src/main/java/com/joseluisgs/todocamara2020/MainActivity.kt
+++ app/src/main/java/com/joseluisgs/todocamara2020/MainActivity.kt
@@ -1,29 +1,68 @@
 package com.joseluisgs.todocamara2020
 
+import android.Manifest
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        iniciarUI()
+        initUI()
     }
 
     /**
      * Inicia la interfaz y los eventos de la apliación
      */
-    private fun iniciarUI() {
+    private fun initUI() {
         // Iniciamos los permisos
-        iniciarPermisos()
+        initPermisos()
     }
 
     /**
      * Comprobamos los permisos de la aplicación
      */
-    private fun iniciarPermisos() {
-        TODO("Not yet implemented")
+    private fun initPermisos() {
+        // Indicamos el permisos y el manejador de eventos de los mismos
+        Dexter.withContext(this)
+            // Lista de permisos a comprobar
+            .withPermissions(
+                Manifest.permission.CAMERA,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+            )
+            // Listener a ejecutar
+            .withListener(object : MultiplePermissionsListener {
+                override fun onPermissionsChecked(report: MultiplePermissionsReport) {
+                    // ccomprbamos si tenemos los permisos de todos ellos
+                    if (report.areAllPermissionsGranted()) {
+                        Toast.makeText(applicationContext, "¡Todos los permisos concedidos!", Toast.LENGTH_SHORT).show()
+                    }
+
+                    // comprobamos si hay un permiso que no tenemos concedido ya sea temporal o permanentemente
+                    if (report.isAnyPermissionPermanentlyDenied) {
+                        // abrimos un diálogo a los permisos
+                        //openSettingsDialog();
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permissions: List<PermissionRequest?>?,
+                    token: PermissionToken
+                ) {
+                    token.continuePermissionRequest()
+                }
+            }).withErrorListener { Toast.makeText(applicationContext, "Existe errores! ", Toast.LENGTH_SHORT).show() }
+            .onSameThread()
+            .check()
     }
 
 
